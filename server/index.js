e27734068f68950902ba1cb06329e13ef73eeb65
David Phan <david.phan@bcgdv.com>
2018-06-06 09:20:05 +0100

    Done TODO: convert hash to ipfsHash

--- server/index.js
+++ server/index.js
@@ -6,11 +6,18 @@ app.get('/', (req, res) => {
     return res.send('Hi guys!');
 })
 
+
+const ipfsHashUtil = require('./ipfshash-bytes32');
+
 app.get('/verify/:hash', (req, res) => {
     let hash = req.params.hash;
     console.log(`verify hash ${hash}`);
 
-    //TODO: convert hash to multihash
+    if (hash.length != 66 && hash.substring(0,2) != '0x') {
+        return res.status(500).send(`not a hash, valid is "0x68371d7e884c168ae2022c82bd837d51837718a7f7dfb7aa3f753074a35e1d87"`);
+    }
+
+    let ipfsHash = ipfsHashUtil.getIpfsHashFromBytes32(hash);
     //TODO : ipfs api get and pin
     //TODO: broadcast blockchain tx to FilesRegistry
 
